c40acae38d315f9bd0f92fbc581b93bbdeba7047
Nikolay Dimov <nikolay.dimov@yara.com>
2024-11-24 09:45:54 +0200

    feat(AwsFileUploadStack): create s3 bucket

--- lib/aws_file_upload-stack.ts
+++ lib/aws_file_upload-stack.ts
@@ -1,16 +1,12 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
 export class AwsFileUploadStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsFileUploadQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        // S3 Bucket
+        const bucket = new s3.Bucket(this, "FileUploadBucketAwsExam");
+    }
 }
