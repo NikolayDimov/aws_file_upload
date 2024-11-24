a8b6348d6a09451044218217af03130e57b25877
Nikolay Dimov <nikolay.dimov@yara.com>
2024-11-24 09:50:17 +0200

    feat(AwsFileUploadStack): dynamoDB table create

--- lib/aws_file_upload-stack.ts
+++ lib/aws_file_upload-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
+import { AttributeType, BillingMode, StreamViewType, Table } from "aws-cdk-lib/aws-dynamodb";
 
 export class AwsFileUploadStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,5 +9,21 @@ export class AwsFileUploadStack extends cdk.Stack {
 
         // S3 Bucket
         const bucket = new s3.Bucket(this, "FileUploadBucketAwsExam");
+
+        // DynamoDB Table for File Metadata
+        const fileMetadataTable = new Table(this, "FileMetadataTable", {
+            partitionKey: {
+                name: "fileId",
+                type: AttributeType.STRING,
+            },
+            sortKey: {
+                name: "uploadDate",
+                type: AttributeType.STRING,
+            },
+            billingMode: BillingMode.PAY_PER_REQUEST,
+            timeToLiveAttribute: "ttl",
+            stream: StreamViewType.NEW_AND_OLD_IMAGES,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+        });
     }
 }
